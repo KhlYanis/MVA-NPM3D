f0252bd34cee00df6b562c5c01ea38b7116457bd
Yaniiiuss <yanis.kahil01@gmail.com>
2025-01-11 17:17:41 +0100

    Q4 done

--- TP1/code/neighborhoods.py
+++ TP1/code/neighborhoods.py
@@ -34,6 +34,8 @@ from ply import write_ply, read_ply
 # Import time package
 import time
 
+import matplotlib.pyplot as plt
+
 
 # ------------------------------------------------------------------------------------------
 #
@@ -50,7 +52,7 @@ def brute_force_spherical(queries, supports, radius):
     # supports est de taille [M, 3] où M est le nombre de points dans le nuage de points
 
     neighborhoods = None
-    
+
     return neighborhoods
 
 
@@ -84,7 +86,7 @@ if __name__ == '__main__':
     #
 
     # Path of the file
-    file_path = './data/indoor_scan.ply'
+    file_path = './TP1/data/indoor_scan.ply'
 
     # Load point cloud
     data = read_ply(file_path)
@@ -97,7 +99,7 @@ if __name__ == '__main__':
     #
 
     # If statement to skip this part if you want
-    if True:
+    if False:
 
         # Define the search parameters
         neighbors_num = 100
@@ -136,13 +138,82 @@ if __name__ == '__main__':
     #
 
     # If statement to skip this part if wanted
-    if False:
+    if True:
+        np.random.seed(42)
 
         # Define the search parameters
-        num_queries = 1000
+        num_queries = 5000
+        radius = 0.20
 
         # YOUR CODE
+        max_leaf = 500
+        step = 25
+        leaf_size = np.arange(1, max_leaf, step)
+
+        build_time, inference_time = [], []
+        random_idx = np.random.choice(points.shape[0], num_queries, replace = False)
+
+        for size in leaf_size :            
+            # Creation of the KDTree
+            t1 = time.time()
+            tree = KDTree(data = points, leaf_size = size)
+            t2 = time.time()
+
+            build_time.append(t2 - t1)
+
+            # Inference
+            t1 = time.time()
+            tree.query_radius(points[random_idx], r = radius)
+            t2 = time.time() 
+
+            inference_time.append(t2 - t1)
+
+        plt.figure()
+        plt.plot(leaf_size, build_time)
+        plt.xlabel('Leaf size')
+        plt.ylabel('Build Time [s]')
+        plt.title(f' {num_queries} queries with r = {radius}')
+        plt.grid()
+        plt.show()
+
+        plt.figure()
+        plt.plot(leaf_size, inference_time, label = "Inference time")
+        plt.xlabel('Leaf size')
+        plt.ylabel('Inference Time [s]')
+        plt.title(f'{num_queries} queries with r = {radius}')
+        plt.grid()
+        plt.show()
+
+        print(f"La valeur optimal de leaf_size pur l'inférence est {leaf_size[np.argmin(inference_time)]}")
+
+
         
+    if True :
+        # Retrieve the optimal leaf number
+        optimal_leaf = 76
         
+        r_values = np.arange(0.05, 1.0, 0.05)
+
+        time_for_radius = []
+        random_idx = np.random.choice(points.shape[0], size = 1000, replace = False)
         
-        
+        for radius in r_values :
+            tree = KDTree(data = points, leaf_size = optimal_leaf)
+            
+            t0 = time.time()
+            tree.query_radius(points[random_idx], r = radius)
+            t1 = time.time()
+
+            time_for_radius.append(t1-t0)
+
+        plt.figure()
+        plt.plot(r_values, time_for_radius)
+        plt.xlabel("Radius [m]")
+        plt.ylabel("Inference time [s]")
+        plt.title(f"Inference time for {1000} queries in function of radius")
+        plt.grid(True)
+        plt.show()
+
+        time_r_20 = time_for_radius[np.argwhere(r_values == 0.20)[0, 0]]
+        total_time_r_20 = (points.shape[0]/num_queries)*time_r_20
+        print(f"Le temps pour r = 20 cm est de {total_time_r_20} secondes")
