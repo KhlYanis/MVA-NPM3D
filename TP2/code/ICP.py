5dd4c24966e8f9aa6b011df6f6e24d026411f88f
maelle19 <maellefontaine53@gmail.com>
2025-01-22 14:42:28 +0100

    question bonus

--- TP2/code/ICP.py
+++ TP2/code/ICP.py
@@ -142,6 +142,63 @@ def icp_point_to_point(data, ref, max_iter, RMS_threshold):
     return data_aligned, R_list, T_list, neighbors_list, RMS_list
 
 
+def icp_point_to_point_fast(data, ref, max_iter, RMS_threshold, sampling_limit):
+    '''
+    Iterative closest point algorithm with a point to point strategy.
+    Inputs :
+        data = (d x N_data) matrix where "N_data" is the number of points and "d" the dimension
+        ref = (d x N_ref) matrix where "N_ref" is the number of points and "d" the dimension
+        max_iter = stop condition on the number of iterations
+        RMS_threshold = stop condition on the distance
+        sampling_limit = number of random points to select at each iteration
+    Returns :
+        data_aligned = data aligned on reference cloud
+        R_list = list of the (d x d) rotation matrices found at each iteration
+        T_list = list of the (d x 1) translation vectors found at each iteration
+        neighbors_list = At each iteration, you search the nearest neighbors of each data point in
+        the ref cloud and this obtain a (1 x N_data) array of indices. This is the list of those
+        arrays at each iteration
+           
+    '''
+
+    # Variable for aligned data
+    data_aligned = np.copy(data)
+
+    # Initiate lists
+    R_list = []
+    T_list = []
+    neighbors_list = []
+    RMS_list = []
+
+    kd_tree = KDTree(ref.T)
+    for i in range(max_iter):
+        data_random = data_aligned[:,np.random.choice(data_aligned.shape[1], sampling_limit, replace=False)]
+        _, neighbors = kd_tree.query(data_random.T, k=1)
+        neighbors = neighbors.squeeze()
+        R, T = best_rigid_transform(data_random, ref[:,neighbors])
+        data_aligned = R @ data_aligned + T
+
+        if i%5 == 0:
+            print('Iteration', i)
+        
+        if i == 0:
+            T_list.append(T)
+            R_list.append(R)
+        else:
+            T_list.append(R @ T_list[-1] + T)
+            R_list.append(R @ R_list[-1])
+
+        neighbors_list.append(neighbors)
+
+        _, all_neighbors = kd_tree.query(data_aligned.T, k=1)
+        #rms = np.sqrt(np.mean(np.sum((data_random - ref[:,neighbors].squeeze())**2, axis=0)))
+        rms = np.sqrt(np.mean(np.sum((data_aligned - ref[:,all_neighbors].squeeze())**2, axis=0)))
+        RMS_list.append(rms)
+    
+        if rms < RMS_threshold:
+            break
+
+    return data_aligned, R_list, T_list, neighbors_list, RMS_list
 
 #------------------------------------------------------------------------------------------
 #
@@ -221,7 +278,7 @@ if __name__ == '__main__':
         
 
     # If statement to skip this part if wanted
-    if True:
+    if False:
 
         # Cloud paths
         bunny_o_path = 'TP2/data/bunny_original.ply'
@@ -243,3 +300,27 @@ if __name__ == '__main__':
         plt.plot(RMS_list)
         plt.show()
 
+    # Bonus
+    # If statement to skip this part if wanted
+    if True:
+
+        # Cloud paths
+        NDC_o_path = 'TP2/data/Notre_Dame_Des_Champs_1.ply'
+        NDC_p_path = 'TP2/data/Notre_Dame_Des_Champs_2.ply'
+        
+        # Load clouds
+        NDC_o_ply = read_ply(NDC_o_path)
+        NDC_p_ply = read_ply(NDC_p_path)
+        NDC_o = np.vstack((NDC_o_ply['x'], NDC_o_ply['y'], NDC_o_ply['z']))
+        NDC_p = np.vstack((NDC_p_ply['x'], NDC_p_ply['y'], NDC_p_ply['z']))
+
+        # Apply ICP
+        #NDC_p_opt, R_list, T_list, neighbors_list, RMS_list = icp_point_to_point_fast(NDC_p, NDC_o, 20, 1e-4, sampling_limit=1000)
+        NDC_p_opt, R_list, T_list, neighbors_list, RMS_list = icp_point_to_point_fast(NDC_p, NDC_o, 20, 1e-4, sampling_limit=15000)
+        
+        # Show ICP
+        # show_ICP(NDC_p, NDC_o, R_list, T_list, neighbors_list)
+        
+        # Plot RMS
+        plt.plot(RMS_list)
+        plt.show()
